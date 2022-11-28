1433ac6ba756e21300ff731c1bba01d8971e8c19
Marcos <jmarcoshenrich@gmail.com>
2022-11-28 15:22:05 -0800

    flaps

--- inheritance.js
+++ inheritance.js
@@ -1,48 +1,32 @@
-
 // Function.prototype.inherits = function(parent) {
 // function Surrogate(){}
 // Surrogate.prototype = parent.prototype
 // this.prototype = new Surrogate()
 // this.prototype.constructor = this
 // }
-
-function MovingObject(name) { 
-    this.name = name;
+function MovingObject() {
 }
-
 function Ship() { }
 // Ship.inherits(MovingObject);
-
 function Asteroid() { }
 // Asteroid.inherits(MovingObject);
-
-MovingObject.prototype.saysHello = function(name) {
+MovingObject.prototype.saysHello = function (name) {
     console.log(`${name} says hello`);
-} 
-
+}
 // m = new MovingObject()
 // s = new Ship()
 // a = new Asteroid()
-
-m = Object.create(() => MovingObject('Andrea'))
-// console.log(m.prototype.saysHello())
-
+m = Object.create(MovingObject.prototype)
+console.log(m.saysHello('Andrea'))
 s = Object.create(Ship, MovingObject)
 a = Object.create(Asteroid, MovingObject)
-
-
-console.log(m.name,",jhfgjk")
+// console.log(m.name,",jhfgjk")
 // m.saysHello('Diego')
 // s.saysHello('Taylor')
 // a.saysHello('Disnee')
-
-Asteroid.prototype.doubler = function(num) {
+Asteroid.prototype.doubler = function (num) {
     console.log(num * 2)
 }
-
 // m.doubler(2)
 // s.doubler(4)
 // a.doubler(6)
-
-
-
